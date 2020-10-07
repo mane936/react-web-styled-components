b49bde38d45d4a1285a33d3ab9a05c450025acd4
Manel <manel.castro39@gmail.com>
2020-10-07 13:18:49 +0200

    setting up styled-components for NavBar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components";
+import GlobalStyle from "./globalStyles";
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+    </Router>
+  );
+}
+
+export default App;
